1caa114cd8da246e5f97c499fe73147b63380ba2
Hamsab31 <h4meed03@gmail.com>
2025-12-19 00:20:28 +0530

    Update prod image repo to onlineshop-prod

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
-docker build -t h4meed/dev:latest .
+#!/bin/bash
+set -e
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$BRANCH" = "dev" ]; then
+  IMAGE="h4meed/dev:latest"
+elif [ "$BRANCH" = "master" ]; then
+  IMAGE="h4meed/onlineshop-prod:latest"
+else
+  echo "Unsupported branch: $BRANCH"
+  exit 1
+fi
+
+echo "Building image: $IMAGE"
+docker build -t $IMAGE .
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push h4meed/dev:latest
+
+docker push $IMAGE
