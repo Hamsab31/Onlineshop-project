873b0a8dc9f4d2b68c87e6c2667e54cd81a0e97b
Hamsab31 <h4meed03@gmail.com>
2025-12-19 00:29:45 +0530

    Fix Jenkins branch detection using GIT_BRANCH

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Jenkins branch: $GIT_BRANCH"
 
-if [ "$BRANCH" = "dev" ]; then
+if [[ "$GIT_BRANCH" == *"dev"* ]]; then
   IMAGE="h4meed/dev:latest"
-elif [ "$BRANCH" = "master" ]; then
+elif [[ "$GIT_BRANCH" == *"master"* ]]; then
   IMAGE="h4meed/onlineshop-prod:latest"
 else
-  echo "Unsupported branch: $BRANCH"
+  echo "Unsupported branch: $GIT_BRANCH"
   exit 1
 fi
 
 echo "Building image: $IMAGE"
+
 docker build -t $IMAGE .
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
